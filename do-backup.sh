b273f817bcba737fdc4ee4a4430c1bb04c94d523
miguelahonen <mika.ahonen@helsinki.fi>
2020-01-21 17:19:02 +0200

    Added one line as a comment

--- do-backup.sh
+++ do-backup.sh
@@ -4,5 +4,6 @@ set -e
 DUMP_FILE=${DUMP_DIRECTORY}/${BACKUP_PREFIX}-`date +%d%m%yT%H%M.tar`
 
 echo -n "Dumping database to ${DUMP_FILE}..."
+#Only this should be changed
 pg_dump -U $DB_USERNAME -F t $DB_NAME > ${DUMP_FILE}
 echo "Done."
